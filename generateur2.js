7d59013e0195b4b89f4125bf2fd6f9cd2f4deb3b
Menasra Mohamed <mohamedmenasradu31@outlook.fr>
2020-09-29 16:29:43 +0200

    ajout du générateur2

---
+++ generateur2.js
@@ -0,0 +1,33 @@
+let tableau1 = ['Végéta ', 'Kaneki ken ', 'Cell '];
+let tableau2 = ['combat','sauve','tue'];
+let tableau3 = [' les méchants',' les terriens',' les démons'];
+
+function creer_phrase(tableau_nom, tableau_verbe, tableau_complement) {
+	let phrase;
+	phrase = tableau_nom[getRandomInt(3)] + tableau_verbe[getRandomInt(3)] + tableau_complement[getRandomInt(3)];
+	return(phrase);
+}
+function getRandomInt(max) {
+  	return(Math.floor(Math.random() * Math.floor(max)));
+}
+function demander_combien() {
+	let combien = prompt("Combien de phrase(s) voulez vous générer (entre 1 et 5) ?");
+	return(combien);
+}
+function afficher_phrases() {
+	let combien = demander_combien();
+	if (combien == 1 || combien == 2 || combien == 3 || combien == 4 || combien == 5) {
+		let count = 0;
+		while(count < combien) {
+			console.log(creer_phrase(tableau1, tableau2, tableau3));
+			count++;
+		}
+	}
+	else {
+		console.log("alerte, la saisie est invalidée");
+	}
+}
+
+afficher_phrases();
+let refresh = document.getElementById("input_recommencer");
+refresh.addEventListener("click", afficher_phrases, false);
