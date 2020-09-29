f54e179f6adfac59e1c8f9e254ee7ba157b12ab6
Menasra Mohamed <mohamedmenasradu31@outlook.fr>
2020-09-29 15:43:32 +0200

    ajout du code dans des fonctions

--- generateur.js
+++ generateur.js
@@ -16,20 +16,27 @@ function getRandomInt(max) {
   	return(Math.floor(Math.random() * Math.floor(max)));
 }
 
-let combien = prompt("Combien de phrase(s) voulez vous générer (entre 1 et 5) ?");
+function demander_combien() {
+	let combien = prompt("Combien de phrase(s) voulez vous générer (entre 1 et 5) ?");
+	return(combien);
+}
 
-if (combien == 1 || combien == 2 || combien == 3 || combien == 4 || combien == 5) {
-	let count = 0;
-	while(count < combien) {
-		console.log(creer_phrase(tableau1, tableau2, tableau3));
-		count++;
+function afficher_phrases(combien) {
+	if (combien == 1 || combien == 2 || combien == 3 || combien == 4 || combien == 5) {
+		let count = 0;
+		while(count < combien) {
+			console.log(creer_phrase(tableau1, tableau2, tableau3));
+			count++;
+		}
+	}
+	else {
+		console.log("alerte, la saisie est invalidée");
 	}
 }
-else {
-	console.log("alerte, la saisie est invalidée");
-}
 
 
+let refresh = document.getElementById("bouton_recommencer");
+refresh.addEventListener("click", afficher_phrases(demander_combien()));
 
 
 
