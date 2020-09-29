5acc9832cb8693ad591bb4b72a2a0e2a7d9d365c
Menasra Mohamed <mohamedmenasradu31@outlook.fr>
2020-09-29 16:26:35 +0200

    ajout fonctionnalités bouton recommencer

--- generateur.js
+++ generateur.js
@@ -1,27 +1,21 @@
 let tableau1 = ['Sangoku ', 'Naruto ', 'Freezer '];
-
 let tableau2 = ['combat','sauve','tue'];
-
 let tableau3 = [' les méchants',' les terriens',' les démons'];
 
 function creer_phrase(tableau_nom, tableau_verbe, tableau_complement) {
 	let phrase;
-
 	phrase = tableau_nom[getRandomInt(3)] + tableau_verbe[getRandomInt(3)] + tableau_complement[getRandomInt(3)];
-	
 	return(phrase);
 }
-
 function getRandomInt(max) {
   	return(Math.floor(Math.random() * Math.floor(max)));
 }
-
 function demander_combien() {
 	let combien = prompt("Combien de phrase(s) voulez vous générer (entre 1 et 5) ?");
 	return(combien);
 }
-
-function afficher_phrases(combien) {
+function afficher_phrases() {
+	let combien = demander_combien();
 	if (combien == 1 || combien == 2 || combien == 3 || combien == 4 || combien == 5) {
 		let count = 0;
 		while(count < combien) {
@@ -34,15 +28,6 @@ function afficher_phrases(combien) {
 	}
 }
 
-
-let refresh = document.getElementById("bouton_recommencer");
-refresh.addEventListener("click", afficher_phrases(demander_combien()));
-
-
-
-
-
-
-
-
-
+afficher_phrases();
+let refresh = document.getElementById("input_recommencer");
+refresh.addEventListener("click", afficher_phrases, false);
